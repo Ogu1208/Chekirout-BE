6c1b744d6f425fc75a68cf225109fa106af09733
Kim._.jazz <jeong011010@naver.com>
2024-10-27 20:01:37 +0900

    chore: 임시 url cors허용

--- src/main/java/com/sch/chekirout/common/config/WebConfig.java
+++ src/main/java/com/sch/chekirout/common/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://chekirout-fe.vercel.app")
+                .allowedOrigins("http://localhost:3000", "https://chekirout-fe.vercel.app", "https://chekirout.pages.dev")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Authorization", "Custom-Header")
